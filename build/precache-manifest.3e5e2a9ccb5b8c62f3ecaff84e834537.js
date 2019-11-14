17baf5b050a285096bc0bf1d515c707f330aa57e
salman <salmannova@gmail.com>
2019-11-14 18:16:09 +0100

    build

---
+++ build/precache-manifest.3e5e2a9ccb5b8c62f3ecaff84e834537.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2563b7dc035637fd6a6b6cf0077b655c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12de7e0a3e86c4abbb76",
+    "url": "/static/css/main.775a0836.chunk.css"
+  },
+  {
+    "revision": "e1caf55180ff75ad208b",
+    "url": "/static/js/2.b45c26a4.chunk.js"
+  },
+  {
+    "revision": "12de7e0a3e86c4abbb76",
+    "url": "/static/js/main.5b5849cf.chunk.js"
+  },
+  {
+    "revision": "b54a3ba0b427d61630a5",
+    "url": "/static/js/runtime-main.9cebec58.js"
+  }
+]);
